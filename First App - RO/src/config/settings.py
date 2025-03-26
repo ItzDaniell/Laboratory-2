78af0a7d7fdf0fd9c528512a46a1283ef6c737a9
ItzDaniell <juan.rodriguez.o@tecsup.edu.pe>
2025-03-26 12:06:04 -0500

    Proyecto Task Manager RodriguezO

--- First App - RO/src/config/settings.py
+++ First App - RO/src/config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,6 +27,7 @@ SECRET_KEY = 'django-insecure-7pj+icrd)9^%g+-f&6jluu5h%#9jz0#2h6^w42vl6qb@ov%9h2
 DEBUG = True
 
 ALLOWED_HOSTS = []
+
 
 
 # Application definition
@@ -119,6 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),  # Ruta a la carpeta de estáticos
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
